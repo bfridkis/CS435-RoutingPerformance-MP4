4653fc9a62549469d227fb729ebc35a6484c71df
bfridkis <bfridkis@gmail.com>
2024-04-05 14:56:01 -0500

    part 2

--- minidc/controller/policy_unmodified.py
+++ minidc/controller/policy_unmodified.py
@@ -173,28 +173,6 @@ class StaticPolicy(object):
         #   (Hint: to find a the VLAN, use topo.getVlanCore(vlanId))
 
         # [ADD YOUR CODE HERE]
-        # create rules for packets from edge -> core (upward)
-        for edge in topo.edgeSwitches.values():
-            routingTable[edge.dpid] = []
-            for h in topo.hosts.values():
-                # don't send edge switch's neighbors up to core
-                if h.name in edge.neighbors:
-                    outport = topo.ports[edge.name][h.name]
-                else:
-                    #find host's vlan (or first one, if multiple)
-                    #print(topo.vlans)
-                    for vlan in topo.vlans.keys():
-                        if h.name in topo.vlans[vlan]:
-                            hostVlan = vlan
-                            break
-                    outport = topo.ports[edge.name][topo.getVlanCore(hostVlan)]
-
-                routingTable[edge.dpid].append({
-                    'eth_dst' : h.eth,
-                    'output' : [outport],
-                    'priority' : 2,
-                    'type' : 'dst'
-                })
 
         return flood.add_arpflood(routingTable, topo)
 
